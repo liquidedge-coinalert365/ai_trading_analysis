4830333e04140b6566be5b009ff2f1da1bb60eb1
clearpal7 <clearpal7@gmail.com>
2025-09-06 12:00:22 +0900

    postcss.config.mjs 수정

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,8 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
-
-export default config;
-
-module.exports = {
+// postcss.config.mjs
+export default {
   plugins: {
     '@tailwindcss/postcss': {},
+    // 필요하면 autoprefixer도 추가하세요(설치되어 있을 때만)
+    // 'autoprefixer': {},
   },
 };
